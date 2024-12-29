fc6a228e5e07cd4addc2acc25c71fa72b79fa724
DWKim67 <dkim67@my.yorku.ca>
2024-12-28 21:32:16 -0500

    Initial Commit

---
+++ Instagram_Copy_Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Instagram_Copy_Demo
+//
+//  Created by Daniel Kim on 2024-12-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instagram_Copy_Demo/Instagram_Copy_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_Copy_DemoApp.swift
+//  Instagram_Copy_Demo
+//
+//  Created by Daniel Kim on 2024-12-28.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_Copy_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
