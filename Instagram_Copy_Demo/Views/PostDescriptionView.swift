b98570c194767a5b1f8ed200bcca6b3b2148f9ba
DWKim67 <dkim67@my.yorku.ca>
2024-12-28 21:56:53 -0500

    Started project folder structure, added incomplete PostDescriptionView

---
+++ Instagram_Copy_Demo/Views/PostDescriptionView.swift
@@ -0,0 +1,18 @@
+//
+//  PostDescriptionView.swift
+//  Instagram_Copy_Demo
+//
+//  Created by Daniel Kim on 2024-12-28.
+//
+
+import SwiftUI
+
+struct PostDescriptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostDescriptionView()
+}
