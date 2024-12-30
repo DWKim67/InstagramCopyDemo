b6d9f994c63fcb7788f585f2db1f2b2a2ec4ab35
DWKim67 <dkim67@my.yorku.ca>
2024-12-30 16:00:11 -0500

    Added comments section pop-up when clicking description, added date to postdescription

---
+++ Instagram_Copy_Demo/Views/CommentsSectionView.swift
@@ -0,0 +1,18 @@
+//
+//  CommentsSectionView.swift
+//  Instagram_Copy_Demo
+//
+//  Created by Daniel Kim on 2024-12-30.
+//
+
+import SwiftUI
+
+struct CommentsSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentsSectionView()
+}
