e4f4ccae5b47031e5719f362d45b1f6d43141c9a
DWKim67 <dkim67@my.yorku.ca>
2024-12-30 11:33:10 -0500

    Fixed issues of post description initialization being broken

--- Instagram_Copy_Demo/ContentView.swift
+++ Instagram_Copy_Demo/ContentView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            PostDescriptionView()
             HeaderView()
             PostImageView(images: [
                 "https://i.imgur.com/DVSDh1m.png",
