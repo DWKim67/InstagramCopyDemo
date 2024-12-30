88d761890c0a0aca114ad94507dea3a4b85233cd
DWKim67 <dkim67@my.yorku.ca>
2024-12-30 11:35:47 -0500

    Added headerview into contentview for preview

--- Instagram_Copy_Demo/ContentView.swift
+++ Instagram_Copy_Demo/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            HeaderView()
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
